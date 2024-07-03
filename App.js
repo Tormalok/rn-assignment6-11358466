7684b96c7bea57d5154fa62204be9ff8f8e1ced3
Henry Amponsah <hamponsah010@st.ug.edu.gh>
2024-07-03 17:17:54 +0000

    made minor changes

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'react-native';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './Screens/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar />
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
       <HomeScreen />
     </View>
   );
